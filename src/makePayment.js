46cdfc58b777510902e3eb7a8fa3ecba975b23a4
Eric Duong <eeoneric@gmail.com>
2018-11-01 08:57:35 -0700

    first commit

--- src/makePayment.js
+++
@@ -1,8 +0,0 @@
-import sendRawTransaction from './sendRawTransaction';
-
-const makePayment = (addressFrom, privateKey, value, _ssAddress, _amountPaid) => {
-	let methodHex = 'makePayment'
-	sendRawTransaction(addressFrom, '0xa9f9fa6c881ef37865a85063ffa03cf4de992b16', privateKey, value, )
-}
-
-export default makePayment
